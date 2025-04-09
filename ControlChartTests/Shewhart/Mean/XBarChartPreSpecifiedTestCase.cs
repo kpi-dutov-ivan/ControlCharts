22079be11c9f87aee06fa9b95103997910c483d9
Ivan Dutov <ivann.dutov@gmail.com>
2025-04-09 11:17:05 +0300

    added tests

---
+++ ControlChartTests/Shewhart/Mean/XBarChartPreSpecifiedTestCase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Business.ControlCharts;
+
+namespace Tests
+{
+    public class XBarChartPreSpecifiedControlChartTestCase : SubgroupControlChartTestCase
+    {
+        public XBarChartPreSpecifiedControlChartTestCase(List<ISubgroup> subgroups, decimal mu0, decimal sigma0,
+            List<decimal> points, decimal centerLine, decimal upperControlLine, decimal lowerControlLine) : base(
+            subgroups, points, centerLine, upperControlLine, lowerControlLine)
+        {
+            Mu0 = mu0;
+            Sigma0 = sigma0;
+        }
+
+        public decimal Mu0 { get; }
+        public decimal Sigma0 { get; }
+    }
+}
